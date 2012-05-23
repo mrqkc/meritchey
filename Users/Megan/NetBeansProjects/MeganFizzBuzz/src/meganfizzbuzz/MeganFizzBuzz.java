4420456349e312dab1e044634e7b0402313585d0
Megan Ritchey <mmritchey2@yahoo.com>
2012-05-23 09:22:43 -0500

    FizzBuzz

---
+++ Users/Megan/NetBeansProjects/MeganFizzBuzz/src/meganfizzbuzz/MeganFizzBuzz.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meganfizzbuzz;
+
+/**
+ *
+ * @author Megan
+ */
+public abstract class MeganFizzBuzz implements FizzBuzz{
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
